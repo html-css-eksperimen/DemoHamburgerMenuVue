819248981928627b6dc1e62547ddbc109b352dd0
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-05-05 07:11:17 +0700

    Init demo data

---
+++ precache-manifest.5c23f389f7740c18e35309c1f292d22a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/robots.txt"
+  },
+  {
+    "revision": "1a7f981c77197d9ad6d8",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/js/hamburger-home.e94a4ded.js"
+  },
+  {
+    "revision": "e4fe51ff3953cfd7ec16",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/js/chunk-vendors.cd457b1e.js"
+  },
+  {
+    "revision": "09e4711a6745348be273",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/js/app.6800d741.js"
+  },
+  {
+    "revision": "62546e40583851cd090d",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/js/about.f424ee4e.js"
+  },
+  {
+    "revision": "0ea1771d95e8614c9c06ffa723e7bbca",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/index.html"
+  },
+  {
+    "revision": "270e62cd86543299122ce8a78ff15ea0",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/img/background_drop_cat.270e62cd.jpg"
+  },
+  {
+    "revision": "1a7f981c77197d9ad6d8",
+    "url": "/html-css-eksperimen/DemoHamburgerMenuVue.git/css/hamburger-home.92162f54.css"
+  }
+];
